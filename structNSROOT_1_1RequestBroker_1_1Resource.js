493e866d7d757326e11e3510faa4007a1f178524
Cookey-dev <84088833+cookey-dev@users.noreply.github.com>
2023-05-20 19:11:49 -0700

    Add files via upload

---
+++ structNSROOT_1_1RequestBroker_1_1Resource.js
@@ -0,0 +1,10 @@
+var structNSROOT_1_1RequestBroker_1_1Resource =
+[
+    [ "contentType", "structNSROOT_1_1RequestBroker_1_1Resource.html#a04544b50e8701c50007a19d15b37691d", null ],
+    [ "delegate", "structNSROOT_1_1RequestBroker_1_1Resource.html#aef5dfee0c6c501428b8c25ff6bfe0f4d", null ],
+    [ "description", "structNSROOT_1_1RequestBroker_1_1Resource.html#a6f97ffbed17f6480b53da254df297e6f", null ],
+    [ "iconUri", "structNSROOT_1_1RequestBroker_1_1Resource.html#ade7ebb1279b744a78695a5d50330a65c", null ],
+    [ "sourcePath", "structNSROOT_1_1RequestBroker_1_1Resource.html#a915ec52d8eb26f8501c6021cef7c099d", null ],
+    [ "title", "structNSROOT_1_1RequestBroker_1_1Resource.html#a7ee1490a9b4b87f0704e5134efb8dad3", null ],
+    [ "uri", "structNSROOT_1_1RequestBroker_1_1Resource.html#abe2aca29f7d4b914b611e9f6a25d0624", null ]
+];
---
+++ structNSROOT_1_1StreamReader_1_1STREAM.js
@@ -0,0 +1,8 @@
+var structNSROOT_1_1StreamReader_1_1STREAM =
+[
+    [ "contentLength", "structNSROOT_1_1StreamReader_1_1STREAM.html#a95936250e492d87c68bbeaa470d720f3", null ],
+    [ "contentType", "structNSROOT_1_1StreamReader_1_1STREAM.html#a95055bb9679654a42f8b66d599319a98", null ],
+    [ "data", "structNSROOT_1_1StreamReader_1_1STREAM.html#a7be10ac63f224a0058694ce7c0d98e4b", null ],
+    [ "opaque", "structNSROOT_1_1StreamReader_1_1STREAM.html#ae4931d0a4e8abac10358f08a426eda46", null ],
+    [ "size", "structNSROOT_1_1StreamReader_1_1STREAM.html#ae4b212d92d1db4a7ffb556ea73d46611", null ]
+];
